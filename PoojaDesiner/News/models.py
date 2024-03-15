74940d511e0dec1e98bf959e316d22a9040c70ad
KurshedKhan <kurshedkhanbhadra.gmail.com>
2024-03-15 09:14:53 +0530

    CRUD Project

--- PoojaDesiner/News/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from tinymce.models import HTMLField
-
-class NewsBox(models.Model):
-    title = models.CharField(max_length=50)
-    content = HTMLField()
-# Create your models here.
--- PoojaDesiner/PoojaDesiner/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class Djangoform(forms.Form):
-
-    num1 = forms.CharField(label='Value1',required=True,widget=forms.TextInput(attrs={"class": "form-control"}))
-    num2 = forms.CharField(label='Value2',required=True,widget=forms.TextInput(attrs={"class": "form-control"}))
-
-    
